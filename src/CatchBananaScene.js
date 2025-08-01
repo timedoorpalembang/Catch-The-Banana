92456aa4363f31ef98a370ca5825ca5f60141a4f
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2025-08-01 17:37:02 +0700

    add background

--- src/CatchBananaScene.js
+++ src/CatchBananaScene.js
@@ -1,29 +1,59 @@
 import Phaser from "phaser";
-export default class CatchBananaScene extends
-Phaser.Scene
-{
-    constructor(){
-        super('catch-banana-scene')
-    }  
-    
-    init(){
 
-    }
-    preload(){
-        this.load.image("background", "images/background.png");
-        this.load.image("badbanana", "images/bad-banana.png");
-        this.load.image("banana", "images/banana.png");
-        this.load.image("startbtn", "images/start-button");
-        this.load.spritesheet('gorilla', 'images/gorilla.png',{
-            frameWidth,
-            frameHeight //lanjutin
-        })
+export default class CatchBananaScene extends Phaser.Scene {
+  constructor() {
+    super("catch-banana-scene");
+  }
 
-    }
-    create(){
+  init() {
+    this.cursor = undefined;
+    this.player = undefined;
+    this.background = undefined;
+  }
 
+  preload() {
+    this.load.image("background", "images/background.jpg");
+    this.load.image("badbanana", "images/bad-banana.png");
+    this.load.image("banana", "images/banana.png");
+    this.load.image("startbtn", "images/start-button.png"); // pastikan file ini ada dan pakai ekstensi!
+    this.load.spritesheet("gorilla", "images/gorilla.png", {
+      frameWidth: 114,
+      frameHeight: 110,
+    });
+  }
+
+  create() {
+    // Tambahkan background terlebih dahulu agar ada di paling bawah
+    this.background = this.add.image(300, 200, "background");
+    this.background.setScrollFactor(0); // Ini mencegah background bergerak saat kamera bergerak
+
+    // Tambahkan gorilla
+    this.player = this.physics.add.sprite(100, 450, "gorilla");
+    this.player.setCollideWorldBounds(true);
+
+    // Input keyboard
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+    // Animasi lompat
+    this.anims.create({
+      key: "jump",
+      frames: this.anims.generateFrameNumbers("gorilla", {
+        start: 12,
+        end: 15,
+      }),
+      frameRate: 10,
+      repeat: 0,
+    });
+  }
+
+  update() {
+    this.background.x -= 2;
+
+    // Gerakan dasar player (kiri, kanan, loncat)
+
+    if (this.cursor.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+      this.player.anims.play("jump", true);
     }
-    update(){
-        
-    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Phaser from "phaser";
+import CatchBananaScene from "./CatchBananaScene";
+
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: []
+    scene: [CatchBananaScene]
 };
 
 var game = new Phaser.Game(config);
